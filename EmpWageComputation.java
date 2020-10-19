3f5a6613d9499b83cb8f7888a1159fc9d76d4b81
prajaktabramhe <prajakta14bramhe@gmail.com>
2020-10-19 23:18:41 +0530

    check the Attendance

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -3,6 +3,14 @@
 public class EmpWageComputation {
 
 	public static void main(String[] args) {
-		System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTATION");
-	}
+			System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTATION");
+		// Constants
+		int IS_FULL_TIME = 1;
+		// Computation
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if (empCheck == IS_FULL_TIME)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
+		}
 }
